89a0c961522da2e79cbab9fe4aa9a7085b5018f0
Bruno <Bruno@DESKTOP-CE08F4J>
2023-08-26 13:36:21 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Forms_Prueba_4
+{
+    partial class Suma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbln1 = new System.Windows.Forms.Label();
+            this.lbln2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnsuma = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbln1
+            // 
+            this.lbln1.AutoSize = true;
+            this.lbln1.Location = new System.Drawing.Point(88, 57);
+            this.lbln1.Name = "lbln1";
+            this.lbln1.Size = new System.Drawing.Size(56, 13);
+            this.lbln1.TabIndex = 0;
+            this.lbln1.Text = "Numero 1:";
+            // 
+            // lbln2
+            // 
+            this.lbln2.AutoSize = true;
+            this.lbln2.Location = new System.Drawing.Point(91, 90);
+            this.lbln2.Name = "lbln2";
+            this.lbln2.Size = new System.Drawing.Size(56, 13);
+            this.lbln2.TabIndex = 1;
+            this.lbln2.Text = "Numero 2:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(151, 57);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(151, 87);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnsuma
+            // 
+            this.btnsuma.Location = new System.Drawing.Point(267, 73);
+            this.btnsuma.Name = "btnsuma";
+            this.btnsuma.Size = new System.Drawing.Size(75, 23);
+            this.btnsuma.TabIndex = 4;
+            this.btnsuma.Text = "Sumar";
+            this.btnsuma.UseVisualStyleBackColor = true;
+            this.btnsuma.Click += new System.EventHandler(this.btnsuma_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(216, 127);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(35, 13);
+            this.lblresultado.TabIndex = 5;
+            this.lblresultado.Text = "label1";
+            // 
+            // Suma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnsuma);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbln2);
+            this.Controls.Add(this.lbln1);
+            this.Name = "Suma";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Suma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbln1;
+        private System.Windows.Forms.Label lbln2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnsuma;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_Prueba_4
+{
+    public partial class Suma : Form
+    {
+        public Suma()
+        {
+            InitializeComponent();
+        }
+
+        private void Suma_Load(object sender, EventArgs e)
+        {
+            lblresultado.Text = "";
+            txt1.Text = "0";
+            txt2.Text = "0";
+        }
+
+        private void btnsuma_Click(object sender, EventArgs e)
+        {
+            int nro1;
+            int nro2;
+            int resultado;
+
+            nro1 = System.Convert.ToInt32(txt1.Text);
+            nro2 = System.Convert.ToInt32(txt2.Text);
+            resultado = nro1 + nro2;
+            lblresultado.Text = System.Convert.ToString(resultado);
+        }
+    }
+}
